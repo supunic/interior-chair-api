9e74a7b5c14f562684fb0f68116ac3791095a8a1
seiya endo <seiya.endo0830@gmail.com>
2022-01-13 14:14:46 +0900

    main.go修正

--- src/main.go
+++ src/main.go
@@ -1,31 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"app/driver/db"
+	"app/driver/web"
 	"github.com/labstack/echo/v4"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-	"net/http"
 )
 
 func main() {
-	dsn := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=Local",
-		"root",
-		"root",
-		"interior_chair_mysql",
-		"3306",
-		"interior_chair_dev",
-	)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(err.Error)
-	}
-	fmt.Println(db)
-
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	conn := db.Init()
+	e := web.Init(echo.New(), conn)
 	e.Logger.Fatal(e.Start(":1323"))
 }
