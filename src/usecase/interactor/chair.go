8b5a0c9b07727acc26d43a0e1422f6ab65065ce8
seiya endo <seiya.endo0830@gmail.com>
2022-01-17 01:52:31 +0900

    createメソッドをinteractorに追加

--- src/usecase/interactor/chair.go
+++ src/usecase/interactor/chair.go
@@ -2,6 +2,7 @@ package interactor
 
 import (
 	"app/entity/model/chair"
+	"app/entity/model/chairAuthor"
 	"app/usecase/port"
 )
 
@@ -19,13 +20,96 @@ func NewChairInputPort(outputPort port.ChairOutputPort, repository port.ChairRep
 
 func (ci *ChairInteractor) FindByID(id int) {
 	chairID, err := chair.NewChairID(id)
-
 	if err != nil {
 		ci.OutputPort.RenderError(err)
 		return
 	}
 
 	c, err := ci.Repository.FindByID(chairID)
+	if err != nil {
+		ci.OutputPort.RenderError(err)
+		return
+	}
+
+	ci.OutputPort.Render(c)
+}
+
+func (ci *ChairInteractor) Create(
+	chairName string,
+	chairFeature string,
+	chairYear int,
+	chairImage string,
+	chairAuthorName string,
+	chairAuthorDescription string,
+	chairAuthorBirthYear int,
+	chairAuthorDiedYear int,
+	chairAuthorImage string,
+) {
+	chairNameObj, err := chair.NewChairName(chairName)
+	if err != nil {
+		ci.OutputPort.RenderError(err)
+		return
+	}
+
+	chairFeatureObj, err := chair.NewChairFeature(chairFeature)
+	if err != nil {
+		ci.OutputPort.RenderError(err)
+		return
+	}
+
+	chairYearObj, err := chair.NewChairYear(chairYear)
+	if err != nil {
+		ci.OutputPort.RenderError(err)
+		return
+	}
+
+	chairImageObj, err := chair.NewChairImage(chairImage)
+	if err != nil {
+		ci.OutputPort.RenderError(err)
+		return
+	}
+
+	chairAuthorNameObj, err := chairAuthor.NewChairAuthorName(chairAuthorName)
+	if err != nil {
+		ci.OutputPort.RenderError(err)
+		return
+	}
+
+	chairAuthorDescriptionObj, err := chairAuthor.NewChairAuthorDescription(chairAuthorDescription)
+	if err != nil {
+		ci.OutputPort.RenderError(err)
+		return
+	}
+
+	chairAuthorBirthYearObj, err := chairAuthor.NewChairAuthorBirthYear(chairAuthorBirthYear)
+	if err != nil {
+		ci.OutputPort.RenderError(err)
+		return
+	}
+
+	chairAuthorDiedYearObj, err := chairAuthor.NewChairAuthorDiedYear(chairAuthorDiedYear)
+	if err != nil {
+		ci.OutputPort.RenderError(err)
+		return
+	}
+
+	chairAuthorImageObj, err := chairAuthor.NewChairAuthorImage(chairAuthorImage)
+	if err != nil {
+		ci.OutputPort.RenderError(err)
+		return
+	}
+
+	c, err := ci.Repository.Create(
+		chairNameObj,
+		chairFeatureObj,
+		chairYearObj,
+		chairImageObj,
+		chairAuthorNameObj,
+		chairAuthorDescriptionObj,
+		chairAuthorBirthYearObj,
+		chairAuthorDiedYearObj,
+		chairAuthorImageObj,
+	)
 
 	if err != nil {
 		ci.OutputPort.RenderError(err)
