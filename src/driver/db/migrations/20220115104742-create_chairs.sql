06ea17b18b6d74056b6d98046dcfefbd8da276f0
seiya endo <seiya.endo0830@gmail.com>
2022-01-16 13:40:11 +0900

    chairsテーブルmigrationファイル修正

--- src/driver/db/migrations/20220115104742-create_chairs.sql
+++ src/driver/db/migrations/20220115104742-create_chairs.sql
@@ -1,8 +1,11 @@
 
 -- +migrate Up
 CREATE TABLE IF NOT EXISTS `chairs` (
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(255) NOT NULL
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '椅子ID',
+    `name` VARCHAR(255) NOT NULL COMMENT '名前',
+    `feature` VARCHAR(255) NOT NULL COMMENT '特徴',
+    `year` INT NOT NULL COMMENT '作成年',
+    `image` VARCHAR(255) NOT NULL COMMENT 'イメージ'
 );
 
 -- +migrate Down
