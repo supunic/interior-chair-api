f6ddcdfe81742afa698da4e7a424f3c5ccfff65e
seiya endo <seiya.endo0830@gmail.com>
2022-01-16 13:40:49 +0900

    chair_authorsテーブルmigrationファイル作成

---
+++ src/driver/db/migrations/20220115125430-create_chair_authors.sql
@@ -0,0 +1,13 @@
+
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS `chair_authoers` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '椅子作者ID',
+    `name` VARCHAR(255) NOT NULL COMMENT '名前',
+    `description` VARCHAR(255) NOT NULL COMMENT '説明',
+    `birth_year` DATE NOT NULL COMMENT '誕生年',
+    `died_year` DATE COMMENT '没年',
+    `image` VARCHAR(255) NOT NULL COMMENT 'イメージ'
+);
+
+-- +migrate Down
+DROP TABLE IF EXISTS chair_authoers;
