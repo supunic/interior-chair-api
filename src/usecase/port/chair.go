782f7a5b4969e285613e2f25b92d58e566117fc6
seiya endo <seiya.endo0830@gmail.com>
2022-01-17 01:52:02 +0900

    createメソッドをportに追加

--- src/usecase/port/chair.go
+++ src/usecase/port/chair.go
@@ -2,10 +2,22 @@ package port
 
 import (
 	"app/entity/model/chair"
+	"app/entity/model/chairAuthor"
 )
 
 type ChairInputPort interface {
 	FindByID(id int)
+	Create(
+		chairName string,
+		chairFeature string,
+		chairYear int,
+		chairImage string,
+		chairAuthorName string,
+		chairAuthorDescription string,
+		chairAuthorBirthYear int,
+		chairAuthorDiedYear int,
+		chairAuthorImage string,
+	)
 }
 
 type ChairOutputPort interface {
@@ -15,4 +27,15 @@ type ChairOutputPort interface {
 
 type ChairRepository interface {
 	FindByID(id *chair.ID) (*chair.Chair, error)
+	Create(
+		chairName *chair.Name,
+		chairFeature *chair.Feature,
+		chairYear *chair.Year,
+		chairImage *chair.Image,
+		chairAuthorName *chairAuthor.Name,
+		chairAuthorDescription *chairAuthor.Description,
+		chairAuthorBirthYear *chairAuthor.BirthYear,
+		chairAuthorDiedYear *chairAuthor.DiedYear,
+		chairAuthorImage *chairAuthor.Image,
+	) (*chair.Chair, error)
 }
