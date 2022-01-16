a1415a89a4e22f1f37e0c6a041177169d5271313
seiya endo <seiya.endo0830@gmail.com>
2022-01-16 19:10:42 +0900

    adapter / controller 修正

--- src/adapter/controller/chair.go
+++ src/adapter/controller/chair.go
@@ -8,23 +8,23 @@ import (
 	"strconv"
 )
 
-type Chair struct {
-	OutputFactory     func(w http.ResponseWriter) port.ChairOutputPort
+type ChairController struct {
 	InputFactory      func(o port.ChairOutputPort, c port.ChairRepository) port.ChairInputPort
+	OutputFactory     func(w http.ResponseWriter) port.ChairOutputPort
 	RepositoryFactory func(c *gorm.DB) port.ChairRepository
 	Conn              *gorm.DB
 }
 
-func (c *Chair) FindByID(ctx echo.Context) error {
+func (cc *ChairController) FindByID(ctx echo.Context) error {
 	id, err := strconv.Atoi(ctx.Param("id"))
 
 	if err != nil {
 		return ctx.JSON(http.StatusBadRequest, err.Error())
 	}
 
-	outputPort := c.OutputFactory(ctx.Response())
-	repository := c.RepositoryFactory(c.Conn)
-	inputPort := c.InputFactory(outputPort, repository)
+	outputPort := cc.OutputFactory(ctx.Response())
+	repository := cc.RepositoryFactory(cc.Conn)
+	inputPort := cc.InputFactory(outputPort, repository)
 	inputPort.FindByID(id)
 
 	return nil
