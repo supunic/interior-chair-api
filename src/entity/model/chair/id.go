435515b6cf3df594c938f1cfca4c85fe464948f9
seiya endo <seiya.endo0830@gmail.com>
2022-01-25 15:08:12 +0900

    idをintからuintに修正

--- src/entity/model/chair/id.go
+++ src/entity/model/chair/id.go
@@ -4,11 +4,11 @@ import "errors"
 
 type ID uint
 
-func NewChairID(val int) (*ID, error) {
+func NewChairID(val uint) (*ID, error) {
 	if val < 1 {
 		return nil, errors.New("idは1以上の整数である必要があります。")
 	}
-	id := ID(uint(val))
+	id := ID(val)
 	return &id, nil
 }
 
