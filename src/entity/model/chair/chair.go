ce6ea055147cb2caa32fa0f7502d5d414a92f6f9
seiya endo <seiya.endo0830@gmail.com>
2022-01-25 14:08:10 +0900

    getter/setter削除

--- src/entity/model/chair/chair.go
+++ src/entity/model/chair/chair.go
@@ -1,33 +1,17 @@
 package chair
 
 import (
-	"app/entity/builder"
 	"app/entity/model/chairAuthor"
 )
 
 type Chair struct {
-	id      ID
-	name    Name
-	feature Feature
-	year    Year
-	image   Image
-	author  chairAuthor.ChairAuthor
-}
-
-func (c *Chair) GetID() *ID                          { return &c.id }
-func (c *Chair) GetName() *Name                      { return &c.name }
-func (c *Chair) GetFeature() *Feature                { return &c.feature }
-func (c *Chair) GetYear() *Year                      { return &c.year }
-func (c *Chair) GetImage() *Image                    { return &c.image }
-func (c *Chair) GetAuthor() *chairAuthor.ChairAuthor { return &c.author }
-
-func (c *Chair) SetID(id int) error {
-	chairID, err := NewChairID(id)
-	if err != nil {
-		return err
-	}
-	c.id = *chairID
-	return nil
+	ID       ID
+	AuthorID chairAuthor.ID
+	Name     Name
+	Feature  Feature
+	Year     Year
+	Image    Image
+	Author   chairAuthor.ChairAuthor
 }
 
 func NewChair(
@@ -58,26 +42,10 @@ func NewChair(
 	}
 
 	return &Chair{
-		name:    *chairName,
-		feature: *chairFeature,
-		year:    *chairYear,
-		image:   *chairImage,
-		author:  *chairAuthor,
+		Name:    *chairName,
+		Feature: *chairFeature,
+		Year:    *chairYear,
+		Image:   *chairImage,
+		Author:  *chairAuthor,
 	}, nil
-}
-
-func (c *Chair) RepositoryData(cn builder.ChairNotification) *builder.Chair {
-	cb := cn.Build()
-
-	cb.Name = c.GetName().Value()
-	cb.Feature = c.GetFeature().Value()
-	cb.Year = c.GetYear().Value()
-	cb.Image = c.GetImage().Value()
-	cb.Author.Name = c.GetAuthor().GetName().Value()
-	cb.Author.Description = c.GetAuthor().GetDescription().Value()
-	cb.Author.BirthYear = c.GetAuthor().GetBirthYear().Value()
-	cb.Author.DiedYear = c.GetAuthor().GetDiedYear().Value()
-	cb.Author.Image = c.GetAuthor().GetImage().Value()
-
-	return cb
 }
