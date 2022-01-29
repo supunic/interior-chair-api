ddcfa07e3f1014dbdd40bbd9180712760ea80194
seiya endo <seiya.endo0830@gmail.com>
2022-01-30 00:14:50 +0900

    chairAuthor ルータ実装

---
+++ src/driver/web/router/chairAuthor.go
@@ -0,0 +1,21 @@
+package router
+
+import (
+	"app/adapter/controller"
+	"github.com/labstack/echo/v4"
+)
+
+type ChairAuthorRouter interface {
+	Register()
+}
+
+type chairAuthorRouter struct {
+	e  *echo.Echo
+	cc controller.ChairAuthorController
+}
+
+func NewChairAuthorRouter(e *echo.Echo, cc controller.ChairController) ChairAuthorRouter {
+	return &chairAuthorRouter{e: e, cc: cc}
+}
+
+func (cr *chairAuthorRouter) Register() {}
