8f8d72e5c39aa7cab2d160d2f2dd3add4e144f72
seiya endo <seiya.endo0830@gmail.com>
2022-01-29 22:51:40 +0900

    id のバリデーション変更

--- src/entity/model/chairAuthor/id.go
+++ src/entity/model/chairAuthor/id.go
@@ -5,8 +5,8 @@ import "errors"
 type ID uint
 
 func NewChairAuthorID(val uint) (*ID, error) {
-	if val < 1 {
-		return nil, errors.New("idは1以上の整数である必要があります。")
+	if val < 0 {
+		return nil, errors.New("idは0以上の整数である必要があります。")
 	}
 	id := ID(val)
 	return &id, nil
