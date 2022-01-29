f071101388011a4bf74914ec2ee2bc1b51d4f9dc
seiya endo <seiya.endo0830@gmail.com>
2022-01-29 21:51:09 +0900

    chair author id を uuid 型に修正

--- src/entity/model/chairAuthor/id.go
+++ src/entity/model/chairAuthor/id.go
@@ -4,7 +4,7 @@ import "errors"
 
 type ID uint
 
-func NewChairAuthorID(val int) (*ID, error) {
+func NewChairAuthorID(val uint) (*ID, error) {
 	if val < 1 {
 		return nil, errors.New("idは1以上の整数である必要があります。")
 	}
